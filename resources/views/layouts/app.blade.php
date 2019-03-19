0e407c357447e3e4b7181f72329e43803a48c71e
anna <hannah19111999@gmail.com>
2019-03-19 16:42:31 +0300

    19-03 16-42

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
 	
     <!-- Title  -->
-    <title> Интернет-магазин часов ""</title>
+    <title> Интернет-магазин часов "8 o'clock" </title>
 
     <!-- Favicon  -->
 	<link href="{{asset('media/img/core-img/favicon.ico')}}" rel="icon" />
